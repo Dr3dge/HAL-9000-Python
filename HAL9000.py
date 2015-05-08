827efe98530eee8f98527144c4fe08a51b05c3b9
Rozza15 <rbuchanan.1997@gmail.com>
2015-05-08 12:21:28 +1000

    Update HAL9000.py
    Trying to debug crash if searchhist.txt does not exist

--- HAL9000.py
+++ HAL9000.py
@@ -9,8 +9,12 @@ from os.path import join
 
 hal9000searchhist = 'searchresults.txt'
 
-with open(hal9000searchhist, 'r') as f:
-        searchhist = f.read()
+if os.path.isfile(hal9000searchhist):
+        with open(hal9000searchhist, 'r') as f:
+                searchhist = f.read()
+else:
+        with open(hal9000searchhist, 'w') as f:
+                f.write('')
 
 def is_search_hist():
     if not os.path.isfile(hal9000searchhist):
