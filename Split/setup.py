681a5d4a2bb88284f136b02164b0b7d8e92e42e5
Rozza15 <rbuchanan.1997@gmail.com>
2015-05-14 19:42:29 +1000

    setup.py update
    updated to show the correct tree

--- Split/setup.py
+++ Split/setup.py
@@ -7,6 +7,6 @@ setup(name='HAL-9000',
       description='HAL-9000 personal assistant',
       author='Rory Buchanan',
       author_email='rbuchanan.1997@gmail.com',
-      url='https://github.com/Dr3dge/HAL-9000-Python/',
+      url='https://github.com/Dr3dge/HAL-9000-Python/tree/master/Split',
       packages=['HAL9000','base','config','miscfunc','name','searching'],
      )
