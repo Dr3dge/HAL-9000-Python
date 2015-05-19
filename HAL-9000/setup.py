379df537febf1d2a1d572ecdc9f5f7a336038972
Rozza15 <rbuchanan.1997@gmail.com>
2015-05-19 21:15:34 +1000

    Changed name from Split to HAL-9000

---
+++ HAL-9000/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HAL-9000',
+    version='v1.0.1a1',
+    packages=[''],
+    url='https://github.com/Dr3dge/HAL-9000-Python',
+    license='GNU GPL 3.0',
+    author='Rozza_15',
+    author_email='rbuchanan.1997@gmail.com',
+    description='HAL-9000 is a Software PA'
+)
